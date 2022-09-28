acfc3e23f3c64bf2979f449a747357a546ba93bf
Gaurab Sarkar <gaurab2193@gmail.com>
2022-09-28 20:44:05 +0530

    calculator

---
+++ pythonDSA/7.ass/index.py
@@ -0,0 +1,40 @@
+# printing in python
+print("Hello")
+
+# addition 
+print("Please enter your name")
+name = input()
+
+print("Please enter first digit")
+firstDigit = int(input())
+print("Please enter second digit")
+secondDigit = int(input())
+
+print("Hi", name, ", your total sum of digits is", firstDigit+secondDigit)
+print()
+
+# subtraction
+print("Please enter first digit")
+firstDigit = int(input())
+print("Please enter second digit")
+secondDigit = int(input())
+
+print("Hi", name, ", your subtration of digits is", firstDigit-secondDigit)
+print()
+
+# division
+print("Please enter first digit")
+firstDigit = int(input())
+print("Please enter second digit")
+secondDigit = int(input())
+
+print("Hi", name, ", your division of digits is", firstDigit/secondDigit)
+print()
+
+# multiplication
+print("Please enter first digit")
+firstDigit = int(input())
+print("Please enter second digit")
+secondDigit = int(input())
+
+print("Hi", name, ", your total multiplication of digits is", firstDigit*secondDigit)
