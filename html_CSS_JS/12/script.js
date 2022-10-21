e3a25e9d309dc1e37ba976c64035b492400790cf
Gaurab Sarkar <gaurab2193@gmail.com>
2022-10-21 11:45:50 +0530

    intro to javascript

---
+++ html_CSS_JS/12/script.js
@@ -0,0 +1,61 @@
+// variable declaration
+var myVariable
+
+// variable definition
+var myVariable = 10
+var myString = 'a'//double quote also works
+
+//multi line comment
+/*
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorum corporis aut ratione nihil, ipsum libero fugit, cumque vero neque unde pariatur natus facere modi voluptatem dolores aliquid! Dolorem, deserunt.
+*/
+
+//Data types
+/*
+boolean // true false
+number // 123 123.23 0.23
+string // 'asd' "asd"
+bigint //
+undefined
+null
+symbol
+*/
+
+//loosely typed programming laguage, you dont have to define which type of data it willhold like integer or string
+
+// multiple variable declaration/definition
+
+var name='Jhon', age, message='Come soon'
+//age value will be undefined
+
+// operators
+/*
+binary operator: need two operands
+ + - / * 
+ power: 2**3 = 8
+remainder: 4%2
+
+unary operator: need only one operand
+2++, 3--, !true
+
+Airthmetic operators:
++ - / * 
+
+Relation operators:
+<, >, <=, >=, !=
+
+Logical operators:
+&&, ||, !
+*/
+
+// truthy and falsy values
+// (boolean logic)
+
+// ===================================
+
+alert('Password is wrong')
+// alert is pausing 
+
+confirm('are you sure?')
+
+prompt('What is your name?')//recieves input from the user
