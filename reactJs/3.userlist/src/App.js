c3d883f452846abb639ae37304d4bc40fa14ce18
Gaurab Sarkar <gaurab2193@gmail.com>
2023-01-12 23:22:16 +0530

    refs

--- reactJs/3.userlist/src/App.js
+++ reactJs/3.userlist/src/App.js
@@ -16,10 +16,10 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </>
   );
 }
 
